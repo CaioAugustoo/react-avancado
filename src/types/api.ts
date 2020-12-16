19ddc3443a4f60fab78fd2dcc6607c90f42e2e8c
caioaugustoo <caioamfr@gmail.com>
2020-12-16 15:39:23 -0300

    Tipagem dos icons das linguagens

--- src/types/api.ts
+++ src/types/api.ts
@@ -17,10 +17,23 @@ export type LandingPageProps = {
   logo: LogoProps
   header: HeaderProps
   sectionAboutProject: SectionAboutProjects
+  sectionTech: SectionTechs
 }
 
 export type SectionAboutProjects = {
   title: string
   description: string
   image: LogoProps 
+}
+
+export type TechIcon = {
+  title: string
+  icon: {
+    url: string
+  }
+}
+
+export type SectionTechs = {
+  title: string
+  techIcons: TechIcon[]
 }
