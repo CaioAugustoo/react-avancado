4e3e71a697a120e9a6ddaf628b2a8b7fc953b2a3
caioaugustoo <caioamfr@gmail.com>
2020-12-16 17:06:30 -0300

    Seção de módulos passado por props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,13 +19,13 @@ import GET_LANDING_PAGE from 'graphql/queries/getLandingPage'
 
 import { LandingPageProps } from 'types/api'
 
-const Index = ({ logo, header, sectionAboutProject, sectionTech, sectionConcepts }: LandingPageProps) => (
+const Index = ({ logo, header, sectionAboutProject, sectionTech, sectionConcepts, sectionModules }: LandingPageProps) => (
   <>
     <SectionHero logo={logo} header={header} />
     <SectionAboutProject {...sectionAboutProject} />
     <SectionTech {...sectionTech} />
-    <SectionConcepts {...sectionConcepts}/>
-    <SectionModules />
+    <SectionConcepts {...sectionConcepts} />
+    <SectionModules {...sectionModules} />
     <SectionAgenda />
     <PricingBox />
     <SectionAboutUs />
