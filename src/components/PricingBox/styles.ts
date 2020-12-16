bfd7d53abe2164434cd483c8cea48c7862e83647
caioaugustoo <caioamfr@gmail.com>
2020-12-16 17:43:08 -0300

    Correção dos estilos dinâmicos

--- src/components/PricingBox/styles.ts
+++ src/components/PricingBox/styles.ts
@@ -59,15 +59,16 @@ export const DiscountPrice = styled.p`
   `}
 `
 
-export const BenefitsList = styled.ul`
-  ${({ theme }) => css`
+export const BenefitsList = styled.div`
+  ul {
+    ${({ theme }) => css`
     list-style: none;
     margin-bottom: ${theme.spacings.large};
   `}
-`
+  }
 
-export const BenefitsItem = styled.li`
-  ${({ theme }) => css`
+  li {
+    ${({ theme }) => css`
     color: ${theme.colors.black};
     font-size: ${theme.font.sizes.small};
     position: relative;
@@ -88,6 +89,8 @@ export const BenefitsItem = styled.li`
       margin-bottom: ${theme.spacings.xsmall};
     }
   `}
+  }
+  
 `
 
 export const ButtonFullPrice = styled.span`
