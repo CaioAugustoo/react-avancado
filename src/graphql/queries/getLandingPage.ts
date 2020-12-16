cd8e7a740a4c1437a3e29447b595d0f037a02eec
caioaugustoo <caioamfr@gmail.com>
2020-12-16 05:29:40 -0300

    Alterada query

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,6 +1,18 @@
 const GET_LANDING_PAGE = /* GraphQL */`
   query GET_LANDING_PAGE {
     landingPage {
+      header {
+        title
+        description
+        button {
+          label
+          url
+        }
+        image {
+          alternativeText
+          url
+        }
+      }
       logo {
         alternativeText
         url
