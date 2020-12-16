ebc09183d363b9ab2cbe8cfe9d452d2837cdd595
caioaugustoo <caioamfr@gmail.com>
2020-12-16 04:54:36 -0300

    Url da imagem e alt vindo por props

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { LogoProps } from 'types/api'
 import * as S from './styles'
 
-const Logo = () => <S.LogoWrapper src="/img/logo.svg" alt="React Avançado" />
+const Logo = ({url, alternativeText}: LogoProps) => <S.LogoWrapper src={`http://localhost:1337${url}`} alt={alternativeText} />
 
 export default Logo
