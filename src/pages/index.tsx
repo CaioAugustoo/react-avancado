289a37ea66d0d35f09da91319fd2bd55b03efb72
caioaugustoo <caioamfr@gmail.com>
2020-12-16 18:35:01 -0300

    Seção de sobre nós passado por props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,16 +19,16 @@ import GET_LANDING_PAGE from 'graphql/queries/getLandingPage'
 
 import { LandingPageProps } from 'types/api'
 
-const Index = ({ logo, header, sectionAboutProject, sectionTech, sectionConcepts, sectionModules, sectionAgenda, pricingBox }: LandingPageProps) => (
+const Index = ({ logo, header, sectionAboutProject, sectionTech, sectionConcepts, sectionModules, sectionAgenda, pricingBox, sectionAboutUs }: LandingPageProps) => (
   <>
     <SectionHero logo={logo} header={header} />
     <SectionAboutProject {...sectionAboutProject} />
     <SectionTech {...sectionTech} />
     <SectionConcepts {...sectionConcepts} />
     <SectionModules {...sectionModules} />
-    <SectionAgenda {...sectionAgenda}/>
+    <SectionAgenda {...sectionAgenda} />
     <PricingBox {...pricingBox} />
-    <SectionAboutUs />
+    <SectionAboutUs {...sectionAboutUs} />
     <SectionReviews />
     <SectionFaq />
     <Footer />
