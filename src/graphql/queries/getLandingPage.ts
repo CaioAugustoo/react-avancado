59c789c62a800781c27f2c4d497dd64da9da7f63
caioaugustoo <caioamfr@gmail.com>
2020-12-16 04:07:41 -0300

    Adição do GraphQL

---
+++ src/graphql/queries/getLandingPage.ts
@@ -0,0 +1,11 @@
+const GET_LANDING_PAGE = /* GraphQL */`
+  query GET_LANDING_PAGE {
+    landingPage {
+      logo {
+        alternativeText
+        url
+      }
+    }
+  }
+`
+export default GET_LANDING_PAGE;
