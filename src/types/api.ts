96d76bf2139121ab80b2ad58d824fd24b9f5a6c5
caioaugustoo <caioamfr@gmail.com>
2020-12-16 05:31:19 -0300

    Tipagem do HeaderProps

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,3 +1,13 @@
+export type HeaderProps = {
+  title: string
+  description: string
+  button: {
+    label: string,
+    url: string
+  }
+  image: LogoProps
+}
+
 export type LogoProps = {
   alternativeText: string
   url: string
@@ -5,4 +15,5 @@ export type LogoProps = {
 
 export type LandingPageProps = {
   logo: LogoProps
+  header: HeaderProps
 }
