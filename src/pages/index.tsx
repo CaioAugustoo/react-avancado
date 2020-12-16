fddb54e5021de60769231a9300c56bc774a37d45
caioaugustoo <caioamfr@gmail.com>
2020-12-16 05:30:47 -0300

    Header passado por props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,9 +20,9 @@ import GET_LANDING_PAGE from 'graphql/queries/getLandingPage'
 
 import { LandingPageProps } from 'types/api'
 
-const Index = ({ logo }: LandingPageProps) => (
+const Index = ({ logo, header }: LandingPageProps) => (
   <>
-    <SectionHero logo={logo}/>
+    <SectionHero logo={logo} header={header} />
     <SectionAboutProject />
     <SectionTech />
     <SectionConcepts />
